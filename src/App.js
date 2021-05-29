a8f88813b55fb2417403b0ae77ac52ea58c34a23
Edio.Mazera <mazera3@gmail.com>
2021-05-29 09:07:55 -0300

    V.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import api from './api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+class App extends Component{
+
+  state = {
+    artigos: [],
+  }
+
+  async componentDidMount(){
+    const response = await api.get('/artigo');
+    
+    // console.log(response.data);
+    
+    this.setState({artigos: response.data})
+
+  }
+
+  render(){
+
+      const {artigos} = this.state;
+      console.log(artigos);
+    return(
+      <div>
+        <h1>Listar Artigos</h1>
+        <ul style={{listStyleType: "none", margin:0, padding:0}}>
+          {artigos.map(artigo => (
+            <li key={artigo._id}>
+              <h3>Titulo: {artigo.titulo}</h3>
+              <p>Conteúdo: {artigo.conteudo}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  };
+};
 
 export default App;
